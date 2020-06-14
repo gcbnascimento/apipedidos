a6452394cbb8adbc9bc697bdb4152c18441641d4
Gustavo Nascimento <gustavoblj@hotmail.com>
2020-06-14 18:24:10 -0300

    Projeto criado

---
+++ src/test/java/com/api/spring/SpringapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
